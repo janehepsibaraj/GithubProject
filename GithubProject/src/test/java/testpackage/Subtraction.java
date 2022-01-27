56d6b5ecfb7b6b19b90e7a441e817863f542dbab
janeautomationtesting2417@gmail.com <MSTSYS@MSTSYS0000443>
2022-01-27 13:52:58 +0530

    commiting second testcse to brancone

---
+++ GithubProject/src/test/java/testpackage/Subtraction.java
@@ -0,0 +1,9 @@
+package testpackage;
+
+public class Subtraction {
+	public static void main(String[] args) {
+        int a = 20, b=10, diff;
+        diff = a+b;
+        System.out.println(diff);
+	}
+}
